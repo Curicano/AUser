7e5b964786f985d73d16ffc0d244c6a02a94a981
Curicano <95356567+Curicano@users.noreply.github.com>
2022-12-23 23:37:09 +1000

    /

--- exe/pyinst.py
+++ exe/pyinst.py
@@ -1,14 +1,14 @@
 import PyInstaller.__main__
 PyInstaller.__main__.run([
-    "D:\Storage X\About\IT\Python\Git\AUser\main.py",
-    "-i=D:\Storage X\About\IT\Python\Git\AUser\image\logo.ico",
-    "--distpath=D:\Storage X\About\IT\Python\Git\AUser\exe",
-    "--workpath=D:\Storage X\About\IT\Python\Git\AUser\exe\Temp",
-    "--specpath=D:\Storage X\About\IT\Python\Git\AUser\exe\Temp",
+    "main.py",
+    "-i=..\..\image\logo.ico",
+    "--distpath=exe",
+    "--workpath=exe\Temp",
+    "--specpath=exe\Temp",
     "-w",
     "-F",
     "-n=AUser",
     "--uac-admin",
-    "--add-data=D:\Storage X\About\IT\Python\Git\AUser\image;image",
-    "--version-file=D:\Storage X\About\IT\Python\Git\AUser\exe\\file_version.py"
+    "--add-data=..\..\image;image",
+    "--version-file=..\..\exe\\file_version.py"
 ])
