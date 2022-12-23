0f612fb4cecd56ab29609a4d0bfb9f1f71d3e595
Curicano <95356567+Curicano@users.noreply.github.com>
2022-12-23 23:04:07 +1000

    /

--- exe/pyinst.py
+++ exe/pyinst.py
@@ -1,17 +1,13 @@
 import PyInstaller.__main__
-# import shutil
 PyInstaller.__main__.run([
     "D:\Storage X\About\IT\Python\Git\AUser\main.py",
     "-i=D:\Storage X\About\IT\Python\Git\AUser\image\logo.ico",
     "--distpath=D:\Storage X\About\IT\Python\Git\AUser\exe",
     "--workpath=D:\Storage X\About\IT\Python\Git\AUser\exe\Temp",
     "--specpath=D:\Storage X\About\IT\Python\Git\AUser\exe\Temp",
-    # "--runtime-tmpdir=.\\",
     "-w",
     "-F",
     "-n=AUser",
     "--uac-admin",
     "--add-data=D:\Storage X\About\IT\Python\Git\AUser\image;image"
 ])
-# shutil.rmtree(
-#    "D:\\Storage X\\About\\Programirovanie\\Python\\Git\\OverlayGT\\Temp")
